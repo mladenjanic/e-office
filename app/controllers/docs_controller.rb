0e6a991c37abdf7d1a38fafcc52fdacc1c5a4176
themladjo <mladenjanic@gmail.com>
2017-06-27 18:12:45 +0200

    Add devise user

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,16 +1,17 @@
 class DocsController < ApplicationController
     before_action :find_doc, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
     
     def index
         @docs = Doc.all.order('created_at DESC')
     end
     
     def new
-        @doc = Doc.new
+        @doc = current_user.docs.build
     end
     
     def create
-        @doc = Doc.new(doc_params)
+        @doc = current_user.docs.build(doc_params)
         
         if @doc.save
             redirect_to @doc
